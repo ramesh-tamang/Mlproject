5ef24d1ecaffaad8314feff06765bfe83c543863
Your Name <your_email@example.com>
2025-11-13 08:09:21 +0545

    Cleaned up unnecessary files and updated project structure

--- setup.py
+++ setup.py
@@ -1,27 +1,27 @@
-from setuptools import find_packages, setup
+from setuptools import find_packages,setup
 from typing import List
 
-HYPEN_E_DOT = "-e ."
-
-def get_requirements(file_path: str) -> List[str]:
-    """
-    This function will return the list of requirements
-    """
-    requirements = []
+HYPEN_E_DOT='-e .'
+def get_requirements(file_path:str)->List[str]:
+    '''
+    this function will return the list of requirements
+    '''
+    requirements=[]
     with open(file_path) as file_obj:
-        requirements = file_obj.readlines()
-        requirements = [req.strip() for req in requirements]  # "\n" हटाउने
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
 
-    if HYPEN_E_DOT in requirements:
-        requirements.remove(HYPEN_E_DOT)
-
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    
     return requirements
 
 setup(
-    name='mlproject',
-    version='0.0.1',
-    author='Ramesh',
-    author_email='ramesh1042637@gmail.com',
-    packages=find_packages(),
-    install_requires=get_requirements('requirements.txt')
+name='mlproject',
+version='0.0.1',
+author='Krish',
+author_email='krishnaik06@gmail.com',
+packages=find_packages(),
+install_requires=get_requirements('requirements.txt')
+
 )
